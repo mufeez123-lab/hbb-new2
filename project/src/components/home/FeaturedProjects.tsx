d78b3b1e314b2f39a138ee5f8435f08714c474d9
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-07 22:39:19 +0530

    update

--- project/src/components/home/FeaturedProjects.tsx
+++ project/src/components/home/FeaturedProjects.tsx
@@ -126,7 +126,7 @@ const FeaturedProjects = () => {
                     </div>
 
                     <div className="p-4 sm:p-6">
-                      <h3 className="text-lg sm:text-xl font-serif font-bold text-primary-800 mb-1">
+                      <h3 className="text-1xl sm:text-xl font-poppins font-bold text-primary-800 mb-1">
                         {project.name}
                       </h3>
                       <div className="text-neutral-500 text-sm mb-2">{project.location}</div>
