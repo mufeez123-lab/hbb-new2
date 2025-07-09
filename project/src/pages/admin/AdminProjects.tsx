a06221c7a4bdde5c07d204b23b9afa61279366a6
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-09 20:17:06 +0530

    front

--- project/src/pages/admin/AdminProjects.tsx
+++ project/src/pages/admin/AdminProjects.tsx
@@ -228,8 +228,9 @@ const AdminProjects: React.FC = () => {
       </div>
 
       {open && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 pt-5 ml-20">
-          <div className="bg-white rounded-lg p-6 w-full max-w-4xl">
+       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+  <div className="bg-white rounded-lg p-6 w-full max-w-4xl max-h-[90vh] overflow-y-auto">
+
             <h2 className="text-xl font-bold mb-4">
               {editingProjectId ? 'Update Project' : 'Add New Project'}
             </h2>
