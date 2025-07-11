ced1d1143c3a4d57fa6aae5c21fed4ef4831f756
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-11 20:54:45 +0530

    front

--- project/src/pages/ProjectDetailPage.tsx
+++ project/src/pages/ProjectDetailPage.tsx
@@ -189,8 +189,8 @@ const ProjectDetailPage = () => {
                     loading="lazy"
                   />
                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">
-  <span className="text-white text-xl bg-black bg-opacity-50 p-1 rounded-full">
-    🔍
+  <span className="text-white text-xl  bg-opacity-50 p-0 ">
+    +
   </span>
 </div>
 
