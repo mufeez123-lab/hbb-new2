2409966407326eac77df966540769aa07a59b13c
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-11 21:44:39 +0530

    v

--- project/src/components/home/Testimonials.tsx
+++ project/src/components/home/Testimonials.tsx
@@ -46,7 +46,7 @@ const Testimonials = () => {
               className="w-full h-full object-cover"
             >
               <source
-                src="https://www.w3schools.com/html/mov_bbb.mp4"
+                src="/test1.mp4"
                 type="video/mp4"
               />
               Your browser does not support the video tag.
