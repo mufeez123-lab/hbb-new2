acc29d19ce21a0138bb1a4ae481d1db8562a9520
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-12 14:07:50 +0530

    brchure

--- project/src/components/home/Hero.tsx
+++ project/src/components/home/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
     infinite: true,
     speed: 1000,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
