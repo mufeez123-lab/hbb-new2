b8e78c318a43c0b1d2f4bf9fc8cb91097e6be424
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-19 17:06:09 +0530

    g

--- project/src/pages/ContactPage.tsx
+++ project/src/pages/ContactPage.tsx
@@ -19,7 +19,7 @@ const ContactPage = () => {
       <div className="pt-20 bg-neutral-50">
         <div className="container mx-auto px-4 md:px-8 py-12">
           <div className="text-center mb-0">
-          <h1 className="text-3xl text-center font-poppins">Contact Us</h1>
+          <h1 className="text-3xl sm:text-2xl  text-center">Contact Us</h1>
           <div className="w-20 h-1 bg-[#8a6c1a] mx-auto mb-6"></div>
 
             <div className="w-20 h-1 bg-dark-500 mx-auto mb-6"></div>
