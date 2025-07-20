ab3f6020f795992898286d6026b39a01e716684b
Mahammad Mufeez <mufizmalar@gmail.com>
2025-07-20 21:00:41 +0530

    r

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { LocomotiveProvider } from './components/utils/LocomotiveProvider';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <LocomotiveProvider>
       <App />
-      </LocomotiveProvider>
     </BrowserRouter>
   </StrictMode>
 );
