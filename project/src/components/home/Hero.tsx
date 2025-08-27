004aacdb9b7a99f70a74178239b18461372fd681
Mahammad Mufeez <123565073+mufeez123-lab@users.noreply.github.com>
2025-08-27 15:05:25 +0000

    heroimagesize

--- project/src/components/home/Hero.tsx
+++ project/src/components/home/Hero.tsx
@@ -34,8 +34,8 @@ const Hero = () => {
     const fetchImages = async () => {
       try {
         const [desktopRes, mobileRes] = await Promise.all([
-          api.get('/hero/desktop'),
-          api.get('/hero/mobile'),
+          api.get('/desktop'),
+          api.get('/mobile'),
         ]);
 
         if (Array.isArray(desktopRes.data.images)) {
