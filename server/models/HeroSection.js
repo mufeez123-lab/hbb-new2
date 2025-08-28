2edc4a27cb5d062e6dab790221cf313f9df41a1d
Mahammad Mufeez <mufizmalar@gmail.com>
2025-08-29 00:59:05 +0530

    Add code from ZI

--- server/models/HeroSection.js
+++ server/models/HeroSection.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-
-
-
 const heroSectionSchema = new mongoose.Schema({
 
   images: [
@@ -37,25 +34,3 @@ const HeroSection = mongoose.model('HeroSection', heroSectionSchema);
 module.exports = HeroSection;
 
 
-const heroSectionSchema = new mongoose.Schema({
-  desktopImages: [
-    {
-      url: { type: String, required: true },
-      public_id: { type: String, required: true },
-    }
-  ],
-  mobileImages: [
-    {
-      url: { type: String, required: true },
-      public_id: { type: String, required: true },
-    }
-  ],
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-}, { timestamps: true });
-
-const HeroSection = mongoose.model('HeroSection', heroSectionSchema);
-module.exports = HeroSection;
->>>>>>> e881c3a32c2c54c08739016776766e447fd074cd
