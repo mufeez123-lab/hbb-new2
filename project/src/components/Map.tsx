148f227f124df25a0215b84277792349c0ac8df6
Mahammad Mufeez <123565073+mufeez123-lab@users.noreply.github.com>
2026-02-07 10:34:21 +0000

    main

--- project/src/components/Map.tsx
+++ project/src/components/Map.tsx
@@ -4,7 +4,7 @@ function Map() {
       <div className="">
         {/* Container for the map with rounded corners and shadow to match your premium theme */}
 <div className="w-full h-[600px]  overflow-hidden shadow-2xl border border-gray-100">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4351.308701311128!2d74.85712171105301!3d12.821064618107764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba35c082b9977c7%3A0x623ce7fcff965c10!2sKINGDOM%20TOWER!5e1!3m2!1sen!2sin!4v1770459382775!5m2!1sen!2sin" width="100%" height="600" style={{ border: "0" }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4351.308701311128!2d74.85712171105301!3d12.821064618107764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba35c082b9977c7%3A0x623ce7fcff965c10!2sKINGDOM%20TOWER!5e1!3m2!1sen!2sin!4v1770459382775!5m2!1sen!2sin" width="100%" height="600" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
 </div>
       </div>
     </section>
